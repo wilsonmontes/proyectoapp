383eb50c69f62a1d130ad0077d6c2b9916b48ccd
wilsonmontes <wimontes06@hotmail.com>
2018-05-12 16:53:11 -0500

    Initial commit

---
+++ proyectoapp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace proyectoapp
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            masterPage.ListView.ItemSelected += ListView_ItemSelected;
+		}
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TipoPagina));
+                masterPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
---
+++ proyectoapp/MasterPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectoapp
+{
+    class MasterPageItem
+    {
+        public string Titulo { get; set; }
+        public string Icono { get; set; }
+        public Type TipoPagina { get; set; }
+    }
+}
